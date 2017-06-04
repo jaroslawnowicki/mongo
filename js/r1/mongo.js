a895cbf40bcc142f50644bbc06f40e6884be06cc
jaroslaw nowicki <jaroslaw.nowicki83@gmail.com>
2017-06-04 20:04:47 +0200

    next day

---
+++ js/r1/mongo.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jarek on 04.06.17.
+ */
+db.posts.find({'tags': 'politics', 'vote_count': {'$gt': 10}});
+use test;
+//switched to db test
+db.users.insert({name: "Kyle"})
+//WriteResult({ "nInserted" : 1 })
+db.users.find()
+//{ "_id" : ObjectId("593449cfd67a70cb5e539c24"), "name" : "Kyle" }
